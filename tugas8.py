0ea481407575cead73883e0da078a12b7c491eb5
gilangzrutz <gilangxd942@gmail.com>
2025-06-13 09:58:34 +0800

    Add files via upload

---
+++ tugas8.py
@@ -0,0 +1,11 @@
+# Definisikan variabel number sebagai string agar bisa diakses per karakter
+number = "1234567890"
+
+# Menampilkan data awal 
+print("Data Awal:", number[0])
+
+# Menampilkan data akhir 
+print("Data Akhir:", number[-1])
+
+# Menampilkan data ke-3 sampai ke-6 
+print("Data ke-3 sampai ke-6:", number[2:6])
